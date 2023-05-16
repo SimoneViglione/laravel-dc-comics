25d29c55ca0806c04e597997e22e7c27319e343f
Simone Viglione <zimoviglio51@gmail.com>
2023-05-16 16:08:08 +0200

    creata migration

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+   <div class="ms_black_banner_footer d-flex justify-content-around align-items-center pb-4">
+        <button type="button" class="btn btn-primary">SIGN-UP NOW!</button>
+        <h6 class="blue-text"> FOLLOW US </h6>
+   </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <div class="ms_blue_banner_header">
+
+    </div>
+
+    <div class="ms_navbar d-flex justify-content-center align-items-center">
+        <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="" class="ms_logo">
+        <ul class="d-flex mt-3">
+            <li class="me-2">
+                <a href="">CHARACTERS</a>
+            </li>
+
+            <li class="me-2">
+                <a href="">COMICS</a>    
+            </li>
+
+            <li class="me-2">
+                <a href="">MOVIES</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="jumbotron">
+        <img src="{{Vite::asset('resources/img/jumbotron.jpg')}}" alt="" class="jumbotron_img">
+    </div>
+</header>
